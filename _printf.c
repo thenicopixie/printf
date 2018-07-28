132199b6bb26ac40742cfd965dd7eef5508ac69f
byn3 <bryanleung92@gmail.com>
2018-07-28 00:52:08 +0000

    added the string copy and putchar helper functions

--- _printf.c
+++ _printf.c
@@ -1,6 +1,37 @@
 #include "holberton.h"
 
 /**
+* _putchar - prints one character
+* @c: a character passed by some function
+*
+* Description: loop through and use this to print a whole thingy
+* Return: a character to the standard output.
+*/
+
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+* _strcpy - copies all the chars including null term
+* @dest: pointer to wehere you want the string copied to
+* @src: the source of the string you want to be copied
+*
+* Description: Copies the string over so we can do stuff
+* Return: Returns the copied over string
+*/
+
+char * _strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] = src[i] != '\0')
+		i++;
+	return (dest);
+}	
+
+/**
 * _strlen - counts how many chars excluding null
 * @s: the string passed to us
 *
