07aaaa98862747e4fec09c10db793ff7a2728dae
thenicopixie <391@holbertonschool.com>
2018-07-28 04:39:25 +0000

    add comments, change variable in if statement from n to dig

--- print_nums.c
+++ print_nums.c
@@ -8,16 +8,20 @@ void print_number(int n)
 	int dig;
 
 	dig = n;
-
+/* if dig is a negative number and have more than 1 digit */
 	if (dig < 0 && dig / 10 == 0)
+/* print '-' */
 		_putchar(45);
-
-	if (n / 10)
+/* if dig is divisible by 10 (more that one digit number) */
+	if (dig / 10)
 	{
+/* divide dig by 10, plug back into fuction */
 		print_number(dig / 10);
 	}
+/* if dig n is a negative number, print sigle digit from dig, multiply by -1 */
 	if (n < 0)
 		_putchar(((dig % 10) * -1) + '0');
+/* else print single digit */
 	else
 		_putchar((dig % 10) + '0');
 }
