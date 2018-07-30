5ada92482ac327d142cdf7c55a116e6b6181bb7f
byn3 <bryanleung92@gmail.com>
2018-07-30 16:20:19 +0000

    added some more cases

--- test/string.c
+++ test/string.c
@@ -10,13 +10,15 @@ int main(void)
     int len;
     int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-  _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("String:[%%]\n");
-    printf("String:[%%]\n");	
+    len = _printf("OURS Let's try to printf a simple sentence.\n");
+    len2 = printf("THEIRS Let's try to printf a simple sentence.\n");
+  _printf("OURS function Character:[%c]\n", 'H');
+    printf("THEIRS standard Character:[%c]\n", 'H');
+    _printf("OURS String:[%s]\n", "I am a string !");
+    printf("THEIRS String:[%s]\n", "I am a string !");
+    _printf("OURS String:[%%]\n");
+    printf("THERIS String:[%%]\n");
+	_printf("OURS length:[%d] and [%d]\n", len, len2);
+	printf("THEIRS length:[%d] and [%d]", len, len2);
 	return (0);
 }
