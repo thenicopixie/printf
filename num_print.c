77f51027cd28150456c0e6f1ab56a83bf150ae22
thenicopixie <391@holbertonschool.com>
2018-07-30 19:58:04 +0000

    make function not recursive

--- num_print.c
+++ num_print.c
@@ -1,22 +1,38 @@
 #include "holberton.h"
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 /**
 * print_number - print integer
 * @n: integer to print
 */
-void print_number(int n)
+int print_number(int n)
 {
-	int dig;
+	int i = 1, digs = n, ct = 0;
 
-	dig = n;
-	if (dig < 0 && dig / 10 == 0)
-		_putchar(45);
-	if (dig / 10)
-		print_number(dig / 10);
-	if (n < 0)
-		_putchar(((dig % 10) * -1) + '0');
-	else
-		_putchar((dig % 10) + '0');
+	while (digs / 10)
+	{
+		digs /= 10;
+		i *= 10;
 	}
+	if (n < 0)
+	{
+		_putchar('-');
+		ct++;
+	}
+	while (i > 0)
+	{
+		digs = n / i;
+		i /= 10;
+		if (digs < 0)
+		{
+			_putchar(digs % 10 * -1 + '0');
+			ct++;
+		}
+		else
+		{
+			_putchar(digs % 10 + '0');
+			ct++;
+		}
+	}
+	return (ct);
+}
