e8f423c2e74f52e04922e86c757060f21b737d89
byn3 <bryanleung92@gmail.com>
2018-07-30 20:37:38 +0000

    refactored so the min int is allowed

--- num_print.c
+++ num_print.c
@@ -10,22 +10,26 @@ int print_number(int n)
 {
 	int dig = n;
 	int count = 0;
-	int places = 1000000000;
-	int x = 0;
+	int places = 1;
 
+	while (dig / 10)
+	{
+		dig /= 10;
+		places *= 10;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		dig = -n;
 		count++;
 	}
-	while (places > dig)
-		places /= 10;
 	while (places > 0)
 	{
-		x = dig / places;
-		_putchar((x % 10) + '0');
+		dig = n / places;
 		places /= 10;
+		if (n < 0)
+			_putchar(dig % 10 * (-1) + '0');
+		else
+			_putchar(dig % 10 + '0');
 		count++;
 	}
 	return (count);
