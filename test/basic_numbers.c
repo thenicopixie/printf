756e046ccb282dd9546afe84b5ebaf9e1a309f76
byn3 <bryanleung92@gmail.com>
2018-07-30 20:20:21 +0000

    changed test cases

--- test/basic_numbers.c
+++ test/basic_numbers.c
@@ -12,8 +12,8 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+    len = _printf("OURS Let's try to printf a simple sentence.\n");
+    len2 = printf("THEIRS Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i, %d, %d] OURS\n", len, len, 69, 69, 69);
