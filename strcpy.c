7eb9c16b0fbd3921bb8a78181c94cd42c7a4d581
byn3 <bryanleung92@gmail.com>
2018-07-30 17:29:09 +0000

    removed trailing white spaces

--- strcpy.c
+++ strcpy.c
@@ -9,12 +9,12 @@
 * Return: Returns the copied over string
 */
 
-char * _strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
 	while (dest[i] && (src[i]) != '\0')
 		i++;
 	return (dest);
-}	
+}
 
