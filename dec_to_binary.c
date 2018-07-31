bdf557efc81dbefd3e36031740434682349aa9e0
byn3 <bryanleung92@gmail.com>
2018-07-31 21:43:22 +0000

    changed the format

--- dec_to_binary.c
+++ dec_to_binary.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
+/**
+* dec_to_binary - converts a dec to binary
+* @n: the number bassed to us
+*
+* Description : does stuff. uses recursion
+* Return: recursion
+*/
+
+
 int dec_to_binary(int n)
 {
-    if (n == 0)
-    {
-        return 0;
-    }	
-    else
-    {
-        return (n % 2 + 10 * dec_to_binary(n / 2));
-    }
+	if (n == 0)
+		return (0);
+	else
+		return (n % 2 + 10 * dec_to_binary(n / 2));
 }
 
