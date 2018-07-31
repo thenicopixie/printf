aa632ae1e8f675cc32f4932e55abc5a4f2349ca5
byn3 <bryanleung92@gmail.com>
2018-07-31 21:23:00 +0000

    code works only for small numbers

--- dec_to_binary.c
+++ dec_to_binary.c
@@ -5,10 +5,10 @@ int dec_to_binary(int n)
     if (n == 0)
     {
         return 0;
-    }
+    }	
     else
     {
-        return (_putchar(n % 2 + 10 * dec_to_binary(n / 2)));
+        return (n % 2 + 10 * dec_to_binary(n / 2));
     }
 }
 
