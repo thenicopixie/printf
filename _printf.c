ba2ecf397a6bb9fff69c16c31951949d2ea7040c
byn3 <bryanleung92@gmail.com>
2018-07-31 03:44:07 +0000

    completely refactored code with use of a hlper function for the format string flags

--- _printf.c
+++ _printf.c
@@ -1,5 +1,69 @@
 #include "holberton.h"
-#include <stdio.h>
+
+/**
+* no_struct - a helpe function that is called when there is a %something
+* @c: the character passed that was after %
+* @count: the number of count thus far. it will be incremented
+* @argu: the va_list that is passed to us so we can va_arg it
+*
+* Description: NO I WILL NOT USE STRUCTS LIKE EVERYONE ELSE
+* Return: the count total
+*/
+
+int no_struct(char c, int count, va_list argu)
+{
+	int j;
+	char *s;
+	int binary;
+
+	switch (c)
+	{
+		case 'c':
+			j = va_arg(argu, int);
+			_putchar(j);
+			count++;
+			break;
+		case 's':
+			s = va_arg(argu, char *);
+			if (!s)
+			{
+				_putchar('(');	
+				_putchar('n');	
+				_putchar('u');	
+				_putchar('l');	
+				_putchar('l');
+				_putchar(')');
+				count += 6;
+			}
+			else
+				count += _putstring(s);
+			break;
+		case 'i':
+		case 'd':
+			j = va_arg(argu, int);
+			if (!j)
+			{
+				count++;
+				_putchar('0');
+			}
+			else
+				count += print_number(j);
+			break;
+		case '%':
+			count++;
+			_putchar('%');
+			break;
+		case 'b':
+			binary = va_arg(argu, unsigned int);
+			count += dec_to_binary(binary);
+			break;
+		default:
+			count += 2;
+			putchar('%');
+			putchar(c);
+	}
+	return (count);
+}
 
 /**
 * _printf - our own printf function
@@ -11,6 +75,36 @@
 
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int count = 0;
+	va_list argu;
+
+	va_start(argu, format);
+
+	if (!format)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			count++;
+			_putchar(format[i]);
+		}
+		else if (format[i + 1])
+		{
+			i++;
+			count = no_struct(format[i], count, argu);
+		}
+		else
+			return (-1);
+	}
+	va_end(argu);
+	return (count);
+}
+
+
+/*
 	va_list argu;
 	int i = 0, ch;
 	int count = 0;
@@ -77,4 +171,5 @@ int _printf(const char *format, ...)
 	}
 	va_end(argu);
 	return (count);
-}
+
+*/
