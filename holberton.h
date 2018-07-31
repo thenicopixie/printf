399b249bcc7e2c740a736830b1086b605fe0e09b
byn3 <bryanleung92@gmail.com>
2018-07-31 03:52:41 +0000

    removed files that were not used

--- holberton.h
+++ holberton.h
@@ -6,12 +6,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-char _putchar(char c);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 int _printf(const char *format, ...);
 int print_number(int n);
-int _strlen_recursion(char *s);
 int dec_to_binary(unsigned int n);
 int _putstring(char *s);
 
